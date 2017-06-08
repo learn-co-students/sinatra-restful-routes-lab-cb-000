9b9fdac113db2f75ca319b3a0e91654f830d8823
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-06-08 10:41:10 +0000

    create recipes table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,14 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
 end
