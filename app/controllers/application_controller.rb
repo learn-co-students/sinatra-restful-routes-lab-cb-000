610f3351ea468a2d3db388c66a374a749df4335b
Sean McGee <sean.a.mcgee@gmail.com>
2017-12-23 20:29:26 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,60 @@
 class ApplicationController < Sinatra::Base
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
+    redirect '/recipes'
+  end
+
+  #Create a new recipe
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.new(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    if @recipe.save
+      redirect "/recipes/#{@recipe.id}"
+    else
+      redirect '/recipes/new'
+    end
+  end
+
+  #Display a single recipe
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  #Edit a single recipe
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect "/recipes/#{params[:id]}"
+  end
+
+  #Display all recipes (index)
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
   end
+
+  #Delete a recipe
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect '/recipes'
+  end
+
 end
