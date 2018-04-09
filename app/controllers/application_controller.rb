61c197e4d0f14f3d4786365f1828a9dc2c4f9c76
stritch.dev <stritch.dev@gmail.com>
2018-04-09 14:03:47 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,55 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+# list
   get '/' do
-    erb :index
+    redirect '/recipes'
+  end
+
+# Index Action
+get '/recipes' do
+  @recipes = Recipe.all
+  erb :index
+end
+
+# New Action
+get '/recipes/new' do
+  erb :new
+end
+
+post '/recipes' do
+  @recipe = Recipe.create(:name => params[:name], 
+                          :ingredients => params[:ingredients],
+                          :cook_time => params[:cook_time])
+  redirect to "/recipes/#{@recipe.id}"
+end
+
+# Show Action
+get '/recipes/:id' do
+  @recipe = Recipe.find_by_id(params[:id])
+  erb :show
+end
+
+# Edit Action
+get '/recipes/:id/edit' do  #load edit form
+  @recipe = Recipe.find_by_id(params[:id])
+  erb :edit
+end
+
+patch '/recipes/:id' do #edit action
+  @recipe = Recipe.find_by_id(params[:id])
+  @recipe.name = params[:name]
+  @recipe.ingredients = params[:ingredients]
+  @recipe.cook_time  = params[:cook_time ]
+  @recipe.save
+  redirect to "/recipes/#{@recipe.id}"
+end
+
+# Delete Action
+  delete '/recipes/:id/delete' do #delete action
+  @recipe = Recipe.find_by_id(params[:id])
+  @recipe.delete
+  redirect to '/recipes'
   end
 end
