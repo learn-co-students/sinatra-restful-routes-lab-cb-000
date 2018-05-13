a690635e3ac2a8bf1d1310b38da912357e655d97
Alex Segers <a.segers.dev@gmail.com>
2018-05-13 03:15:57 +0000

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+Recipe.destroy_all
+
+Recipe.create!([{
+    name: "Spaghetti",
+    ingredients: "Meatballs, Noodles, Tomato Sauce",
+    cook_time: "30 minutes"
+  },
+  {
+    name: "PB&J",
+    ingredients: "Bread, Peaunut Butter, Jelly/Jam/Perserves",
+    cook_time: "none"
+  },
+  {
+    name: "Popcorn",
+    ingredients: "Corn Kernels, Butter, Salt",
+    cook_time: "3 minutes"
+}])
+
+puts "Created #{Recipe.count} recipies"
