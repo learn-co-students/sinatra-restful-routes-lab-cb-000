8c565f424e3c0802b77cea29348725775ddff1b1
Liz Jenerson <liz.jenerson@gmail.com>
2018-05-25 18:14:38 +0000

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+recipe1 = Recipe.create(name: "Chocolate Chip Cookies", ingredients: "milk, dough, chocolate chips", cook_time: 30)
+
+recipe2 = Recipe.create(name: "Eggs", ingredients: "eggs, cheese", cook_time: 5)
