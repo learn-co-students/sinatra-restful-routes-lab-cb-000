540576f8eedb0e4bf2b347a30c87b52ff1eff0bc
Kevin K <doilerjar@outlook.com>
2018-08-25 04:51:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,25 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
   get '/' do
     erb :index
+  end
+  
+  get '/recipes' do
+    erb :recipes
+    
+  end
+  
+  patch '/recipes/:id' do
+    erb :recipes_id
+    
+  end
+  
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.destroy
+    
+    redirect '/recipes'
   end
 end
